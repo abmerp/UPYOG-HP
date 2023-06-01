386a77c794b66ed520d033dd5d555e7cc1706c8f
palamgarg <palamgarg@gmail.com>
2023-06-01 13:15:47 +0530

    Update BPAService.java
    Added back code

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/service/BPAService.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/service/BPAService.java
@@ -149,15 +149,15 @@ public class BPAService {
 		RequestInfo requestInfo = bpaRequest.getRequestInfo();
 		String tenantId = bpaRequest.getBPA().getTenantId();
 		log.info(tenantId);
-	//	Object mdmsData = util.mDMSCall(requestInfo, tenantId);
+		Object mdmsData = util.mDMSCall(requestInfo, tenantId);
 		String serviceType = bpaRequest.getBPA().getServiceType();
 		Map<String, String> values = new HashMap<String, String>();
 //		values.put(BPAConstants.SERVICETYPE, "Form_12");
 //		String applicationType = ;
 		values.put(BPAConstants.APPLICATIONTYPE, bpaRequest.getBPA().getApplicationType());
 		String applicationType = values.get(BPAConstants.APPLICATIONTYPE);
-	//	bpaValidator.validateCreate2(bpaRequest, mdmsData, values);
-	//	enrichmentService.enrichBPACreateRequest2(bpaRequest, mdmsData, values);
+		bpaValidator.validateCreate2(bpaRequest, mdmsData, values);
+		enrichmentService.enrichBPACreateRequest2(bpaRequest, mdmsData, values);
 		wfIntegrator.callWorkFlow2(bpaRequest);
 		if(bpaRequest.getBPA().getDepartment().equals(BPAConstants.DEPARTMENT_TCP) &&
 				bpaRequest.getBPA().getApplicationType().equalsIgnoreCase(BPAConstants.APPLICATION_TYPE_FORM_26)) {
