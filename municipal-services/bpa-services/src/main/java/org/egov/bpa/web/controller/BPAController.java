044326cea9e34f7ef45c64e985f4db46da4eaf05
SuryodayABM <suryoday.yadav@abmindia.com>
2023-06-12 12:21:15 +0530

    Committing Satyamashish changes for allowing croS origin request

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/web/controller/BPAController.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/web/controller/BPAController.java
@@ -35,6 +35,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/bpa")
 public class BPAController {
 
